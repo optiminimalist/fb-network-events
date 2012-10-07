48512f72729409d79689c644f2ec171701011cc9
Michael Luckeneder <michael@luckeneder.net>
2012-10-07 19:29:10 +0100

    changed readme and fetch

--- fetch.rb
+++ fetch.rb
@@ -5,29 +5,24 @@ require 'fql'
 require './config'
 
 options = {access_token: FB_ACCESS_TOKEN}
+location_matches = ["universityofstandrews", "standrews", "st.andrews", "saintandrews"]
 
+friends =  Fql.execute('SELECT uid, name, affiliations, current_location, education FROM user WHERE uid=me() OR uid IN (SELECT uid2 FROM friend WHERE uid1 = me())',options)
 
-friends =  Fql.execute('SELECT uid, name, affiliations, current_location, education FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())',options)
-puts friends
-
-friends = friends.find_all { |e| 
-   !e["affiliations"].map{|e| e["nid"]}.include? 16777588
+friends = friends.find_all { |f| 
+   (f["affiliations"].map{|e| e["nid"]}.include? 16777588) || (f["education"].map{|e| e["school"]["id"]}.include? 16777588)
 }
+uids = friends.map{|f| f["uid"]}.join(",")
 
-puts friends.map{|f| f["education"]}.inspect
-exit
 
 events = {}
-friends[0,30].each_with_index do |f,index|
-  puts "(#{index+1}/#{friends.count}) fetching events from #{f['name']}"
-  ev = Fql.execute("SELECT name,eid,start_time,end_time, host, description, creator, location, privacy, venue FROM event WHERE eid IN (SELECT eid from event_member WHERE uid = #{f['uid']})", options)
-  
-  ev.each do |e|
-    if e["location"]
-      location = e["location"].downcase.gsub(/\s/, "") 
-      if ["universityofstandrews", "standrews", "st.andrews"].any? { |w| location =~ /#{w}/ }
-        events[e["eid"]] = e
-      end
+puts "fetching events..."
+ev = Fql.execute("SELECT name, eid, start_time, end_time, host, description, creator, location, privacy, venue FROM event WHERE eid IN (SELECT eid from event_member WHERE uid IN (#{uids})) AND privacy='open'", options)
+ev.each do |e|
+  if e["location"]
+    location = e["location"].downcase.gsub(/\s/, "") 
+    if location_matches.any? { |w| location =~ /#{w}/ }
+      events[e["eid"]] = e
     end
   end
 end
